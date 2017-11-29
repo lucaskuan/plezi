fe72b77170388ed8c4427abe5ddfffb15c062182
Lucas <kahou526@gmail.com>
2017-11-29 22:13:19 +0800

    test middleware 3

--- lib/plezi/router/route.rb
+++ lib/plezi/router/route.rb
@@ -36,7 +36,7 @@ module Plezi
             params = match(request.path_info, request)
             return nil unless params
             c = @controller.new
-            puts "Initialize controller #{c}"
+            puts "Initialize controller #{c.class}"
             c._pl_respond(request, response, params)
          end
 
